dae43f3e60b093456a48e0a217c5a4727420a613
Daniel Fischer <d.fischer874@gmail.com>
2020-02-11 06:52:59 -0500

    Changed OptionUtilTest and StreamTest to be actual tests

--- src/test/scala/fp/option/OptionUtilTest.scala
+++ src/test/scala/fp/option/OptionUtilTest.scala
@@ -1,18 +1,21 @@
 package fp.option
 
 import fp.list._
+import org.scalatest.FunSuite
 
-object OptionUtilTest {
+class OptionUtilTest extends FunSuite {
 
-  def main(args: Array[String]): Unit = {
-    println(OptionUtil.map2(Some(2), Some(2))((a, b) => a + b))
-    println(OptionUtil.map2(None, Some(2))((a: Int, b) => a + b))
-    println(OptionUtil.map2(Some(2), None)((a, b: Int) => a + b))
-    println(OptionUtil.map2(None, None)((a: Int, b: Int) => a + b))
+  test("map2") {
+    assert(OptionUtil.map2(Some(2), Some(2))((a, b) => a + b) == Some(4))
+    assert(OptionUtil.map2(None, Some(2))((a: Int, b) => a + b) == None)
+    assert(OptionUtil.map2(Some(2), None)((a, b: Int) => a + b) == None)
+    assert(OptionUtil.map2(None, None)((a: Int, b: Int) => a + b) == None)
+  }
 
-    println(OptionUtil.sequence(Cons(Some(4), Cons(Some(34), Cons(Some(8), Nil)))))
-    println(OptionUtil.sequence(Cons(Some(4), Cons(Some(34), Cons(None, Nil)))))
-    println(OptionUtil.sequence(Cons(None, Cons(Some(34), Cons(Some(8), Nil)))))
+  test("sequence") {
+    assert(OptionUtil.sequence(Cons(Some(4), Cons(Some(34), Cons(Some(8), Nil)))) == Some(Cons(4, Cons(34, Cons(8, Nil)))))
+    assert(OptionUtil.sequence(Cons(Some(4), Cons(Some(34), Cons(None, Nil)))) == None)
+    assert(OptionUtil.sequence(Cons(None, Cons(Some(34), Cons(Some(8), Nil)))) == None)
   }
 
 }
--- src/test/scala/fp/stream/StreamTest.scala
+++ src/test/scala/fp/stream/StreamTest.scala
@@ -1,66 +1,26 @@
 package fp.stream
 
-object StreamTest {
+import org.scalatest.FunSuite
 
-  def main(args: Array[String]): Unit = {
-    val a = Stream(
-      {println(1); 1},
-      {println(2); 2},
-      {println(3); 3})
-    println(a)
-    println(a.toList)
+class StreamTest extends FunSuite {
 
-    println("Taking 1")
-    val b = Stream(
-      {println(1); 1},
-      {println(2); 2},
-      {println(3); 3})
-    println(b.take(1).toList)
+  test("Stream") {
+    assert(Stream(1, 2, 3).toList == List(1, 2, 3))
+  }
 
-    println("Taking 2")
-    val c = Stream(
-      {println(1); 1},
-      {println(2); 2},
-      {println(3); 3})
-    println(c.take(2).toList)
+  test("take") {
+    assert(Stream(1, 2, 3).take(1).toList == List(1))
+    assert(Stream(1, 2, 3).take(2).toList == List(1, 2))
+  }
 
-    println("Dropping 2")
-    val d = Stream({
-      println(1); 1},
-      {println(2); 2},
-      {println(3); 3})
-    println(d.drop(2).toList)
+  test("drop") {
+    assert(Stream(1, 2, 3).drop(2).toList == List(3))
+    assert(Stream(1, 2, 3, 4, 5, 6).drop(3).toList == List(4, 5, 6))
+  }
 
-    println("Dropping 3")
-    val e = Stream(
-      {println(1); 1},
-      {println(2); 2},
-      {println(3); 3},
-      {println(4); 4},
-      {println(5); 5},
-      {println(6); 6})
-    println(e.drop(3).toList)
-
-    println("take while even")
-    val f = Stream(
-      {println(2); 2},
-      {println(4); 4},
-      {println(6); 6},
-      {println(7); 7},
-      {println(8); 8},
-      {println(10); 10})
-    println(f.takeWhile(e => e % 2 == 0).toList)
-
-    println("take while even 2")
-    val g = Stream(
-      {println(1); 1},
-      {println(2); 2},
-      {println(4); 4},
-      {println(6); 6},
-      {println(7); 7},
-      {println(8); 8},
-      {println(10); 10})
-    println(g.takeWhile(e => e % 2 == 0).toList)
+  test("takeWhile") {
+    assert(Stream(2, 4, 6, 7, 8, 10).takeWhile(x => x % 2 == 0).toList == List(2, 4, 6))
+    assert(Stream(1, 2, 4, 6, 7, 8, 10).takeWhile(x => x % 2 == 0).toList == List())
   }
 
 }
