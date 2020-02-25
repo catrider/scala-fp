f9912e1d8d2410d6074fe5aff19f21dcdaff4a17
Daniel Fischer <d.fischer874@gmail.com>
2020-02-25 07:32:53 -0500

    Implemented unit, map, nonNegativeEven, and mapDouble in SimpleRNGUtils

--- src/main/scala/fp/random/SimpleRNGUtils.scala
+++ src/main/scala/fp/random/SimpleRNGUtils.scala
@@ -1,6 +1,8 @@
 package fp.random
 
-object SimpleRNGExercises {
+object SimpleRNGUtils {
+
+  type Rand[+A] = RNG => (A, RNG)
 
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
     val (number, nextRNG) = rng.nextInt
@@ -22,6 +24,26 @@ object SimpleRNGExercises {
     val (i, rng1) = nonNegativeInt(rng)
     val (d, rng2) = double(rng1)
     ((d, i), rng2)
+  }
+
+  def unit[A](a: A): Rand[A] = {
+    rng => (a, rng)
+  }
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] = {
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+  }
+
+  def nonNegativeEven: Rand[Int] = {
+    map(nonNegativeEven)(i => i - i % 2)
+  }
+
+  def mapDouble(rng: RNG): (Double, RNG) = {
+    val doubleRng = map(rng => rng.nextInt)(a => Math.abs(a.toDouble / Int.MaxValue.toDouble))
+    doubleRng(rng)
   }
 
 }
--- src/test/scala/fp/random/SimpleRNGUtilsTest.scala
+++ src/test/scala/fp/random/SimpleRNGUtilsTest.scala
@@ -3,22 +3,22 @@ package fp.random
 import fp.stream.StreamUtils.unfold
 import org.scalatest.FunSuite
 
-class SimpleRNGExercisesTest extends FunSuite {
+class SimpleRNGUtilsTest extends FunSuite {
 
   test("nonNegativeInt") {
-    assert(unfold(SimpleRNG(4):RNG)(rng => Some(SimpleRNGExercises.nonNegativeInt(rng)))
+    assert(unfold(SimpleRNG(4):RNG)(rng => Some(SimpleRNGUtils.nonNegativeInt(rng)))
       .take(100)
       .forAll(i => i >= 0))
   }
 
   test("double") {
-    assert(unfold(SimpleRNG(4):RNG)(rng => Some(SimpleRNGExercises.double(rng)))
+    assert(unfold(SimpleRNG(4):RNG)(rng => Some(SimpleRNGUtils.double(rng)))
       .take(100)
       .forAll(i => i >= 0 && i <= 1))
   }
 
   test("intDouble") {
-    assert(unfold(SimpleRNG(4):RNG)(rng => Some(SimpleRNGExercises.intDouble(rng)))
+    assert(unfold(SimpleRNG(4):RNG)(rng => Some(SimpleRNGUtils.intDouble(rng)))
       .take(100)
       .forAll(result => {
         val (i, d) = result
@@ -27,12 +27,18 @@ class SimpleRNGExercisesTest extends FunSuite {
   }
 
   test("doubleInt") {
-    assert(unfold(SimpleRNG(4):RNG)(rng => Some(SimpleRNGExercises.doubleInt(rng)))
+    assert(unfold(SimpleRNG(4):RNG)(rng => Some(SimpleRNGUtils.doubleInt(rng)))
       .take(100)
       .forAll(result => {
         val (d, i) = result
         i >= 0 && d >= 0 && d <= 1
       }))
+  }
+
+  test("mapDouble") {
+    assert(unfold(SimpleRNG(4):RNG)(rng => Some(SimpleRNGUtils.mapDouble(rng)))
+      .take(100)
+      .forAll(i => i >= 0 && i <= 1))
   }
 
 }
